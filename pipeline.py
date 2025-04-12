3093f303a4dcbe350500447aaba49d580db80eb8
mhtruong1031 <mhtruong1031@gmail.com>
2025-04-12 16:59:17 -0700

    init commit

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+import json
+
+
+def process_annotations(image_path: str, url_link: str):
+    # load image
+
+    # process and return relevant information (image, text)
+
+        # extract text and sort into categories, position on page, overall topic
+            # beautifulsoup scrape web page for text
+
+        # locate and identify annotations
+            # TO-DO: self-trained or pretrained+finetuned
+
+    pass
