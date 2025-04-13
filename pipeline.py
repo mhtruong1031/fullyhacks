2877e8a3541a88eb15398fd7f54172794591db3f
mhtruong1031 <mhtruong1031@gmail.com>
2025-04-12 18:03:58 -0700

    added requirements.txt

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,33 @@
-import json
+import cv2
+import easyocr
+
 from bs4 import BeautifulSoup as bs
+
+reader = easyocr.Reader(['en'])
+
+# Extracts text from an image and returns 
+def extract_text(image_path: str) -> list[str]:
+    image = cv2.imread(image_path)
+
+    # Preprocess image
+    gray         = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    text_results = reader.readtext(gray)
+
+    print(text_results)
+
+    # Find and group text
+    lines = []
+    for (bbox, word, conf) in text_results:
+        if len(lines) == 0:
+            lines.append([(bbox, word, conf)])
+            continue
+
+        
+        for l in lines:
+            
+
+
+
 
 def process_annotations(image_path: str, url_link: str):
     # load image
@@ -16,6 +44,4 @@ def process_annotations(image_path: str, url_link: str):
 
     pass
 
-# Extracts 
-def extract_text(image_path: str) -> list[str]:
-    
+extract_text('test.png')
