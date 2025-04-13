3e4fd418455e5e5ae13868d3a8aaafd207ec5220
mhtruong1031 <mhtruong1031@gmail.com>
2025-04-12 17:22:30 -0700

    balls

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,21 @@
 import json
-
+from bs4 import BeautifulSoup as bs
 
 def process_annotations(image_path: str, url_link: str):
     # load image
 
     # process and return relevant information (image, text)
 
+
         # extract text and sort into categories, position on page, overall topic
             # beautifulsoup scrape web page for text
+            
 
         # locate and identify annotations
             # TO-DO: self-trained or pretrained+finetuned
 
     pass
+
+# Extracts 
+def extract_text(image_path: str) -> list[str]:
+    
